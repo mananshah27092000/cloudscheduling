98828d87558fe5fc57e3f6b8749f81960c582f98
Manul Goyal <manul.goyal@gmail.com>
2021-11-06 01:10:03 +0530

    started working on ACO

---
+++ hyperhueristicscheduling/org/cloudbus/cloudsim/hyperhueristicscheduling/metaheuristics/antcolony/AntColonyParameters.java
@@ -0,0 +1,11 @@
+package org.cloudbus.cloudsim.hyperhueristicscheduling.metaheuristics.antcolony;
+
+public class AntColonyParameters {
+    public double evaporationRate;
+    public double pheromoneWeight;
+    public double heuristicWeight;
+    public double pheromoneUpdationRate;
+    public int maxGenerations;
+    public int antsPerGeneration;
+    public double initPheromone;
+}
